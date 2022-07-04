8486691044fe7ca4b57a770f8a5227b6213ebe8c
CamielVilla <camielvilla@gmail.com>
2022-07-04 09:06:12 +0200

    final push

--- src/main/java/nl/novi/assigment/homecare/controller/WoundExaminationController.java
+++ src/main/java/nl/novi/assigment/homecare/controller/WoundExaminationController.java
@@ -1,34 +1,14 @@
 package nl.novi.assigment.homecare.controller;
-import nl.novi.assigment.homecare.model.dto.WoundDto;
-import nl.novi.assigment.homecare.model.entity.FileUploadResponse;
-import nl.novi.assigment.homecare.model.dto.CreateWoundExaminationDto;
-import nl.novi.assigment.homecare.model.dto.WoundExaminationDto;
-import nl.novi.assigment.homecare.model.entity.WoundExamination;
 import nl.novi.assigment.homecare.service.WoundExaminationService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.net.URI;
-import java.util.List;
-import java.util.Set;
 
 @CrossOrigin
 @RestController
 @RequestMapping("woundexaminations")
 public class WoundExaminationController {
-
-    private final WoundExaminationService woundExaminationService;
+private final WoundExaminationService woundExaminationService;
 
     public WoundExaminationController(WoundExaminationService woundExaminationService) {
         this.woundExaminationService = woundExaminationService;
     }
-
-    @GetMapping
-    public ResponseEntity<List<WoundExaminationDto>> getAllExams(){
-        return ResponseEntity.ok(woundExaminationService.getAllWoundExamination());
-    }
-
-
-
 }
